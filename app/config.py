303446d98b49a079b7d9f206768ca51a36b27b25
new333111 <new333713@gmail.com>
2026-07-09 01:12:23 +0300

    update app/config.py

---
+++ app/config.py
@@ -0,0 +1,14 @@
+class Config:
+    # معلومات التطبيق
+    APP_NAME = "Android Template"
+    VERSION = "0.1.0"
+
+    # الثيم
+    THEME_STYLE = "Light"
+    PRIMARY_PALETTE = "Blue"
+
+    # شاشة البداية
+    SPLASH_TIME = 2
+
+    # اللغة
+    LANGUAGE = "en"
