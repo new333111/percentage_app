52563c6a38b764d2abfec2c8cc8a3017b89de50c
new333111 <new333713@gmail.com>
2026-07-09 01:06:13 +0300

    update app/screens/manager.py

--- app/screens/manager.py
+++ app/screens/manager.py
@@ -1,6 +1,13 @@
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
+
+from app.screens.home import HomeScreen
 
 
 class AppManager(ScreenManager):
-    """مدير جميع شاشات التطبيق"""
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.transition = FadeTransition()
+
+        self.add_widget(HomeScreen())
