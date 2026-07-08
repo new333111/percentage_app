153557c64667b9676a7cd27b4c47f266e304222d
new333111 <new333713@gmail.com>
2026-07-09 01:16:30 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from kivymd.app import MDApp
 
+from app.config import Config
 from app.utils.loader import load_kv_files
 from app.screens.manager import AppManager
 
 
-class PercentageApp(MDApp):
+class AndroidTemplateApp(MDApp):
 
     def build(self):
-
-        self.title = "Percentage App"
-
-        self.theme_cls.primary_palette = "Blue"
-
-        self.theme_cls.theme_style = "Light"
-
+        # تحميل جميع ملفات KV
         load_kv_files()
 
+        # معلومات التطبيق
+        self.title = Config.APP_NAME
+
+        # إعداد الثيم
+        self.theme_cls.theme_style = Config.THEME_STYLE
+        self.theme_cls.primary_palette = Config.PRIMARY_PALETTE
+
+        # إنشاء مدير الشاشات
         return AppManager()
 
 
 if __name__ == "__main__":
-    PercentageApp().run()
+    AndroidTemplateApp().run()
