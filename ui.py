513779d29f12c04aeb278195859717aaaabeded9
new333111 <new333713@gmail.com>
2026-07-08 23:29:37 +0300

    update ui.py

--- ui.py
+++ ui.py
@@ -4,49 +4,69 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 
-class MainLayout(BoxLayout):
+class MainUI(BoxLayout):
 
     def __init__(self, **kwargs):
-        super().__init__(orientation="vertical", spacing=10, padding=20, **kwargs)
+        super().__init__(**kwargs)
 
-        self.amount = TextInput(
-            hint_text="Amount",
+        self.orientation = "vertical"
+        self.padding = 20
+        self.spacing = 15
+
+
+        title = Label(
+            text="Percentage Calculator",
+            font_size=30
+        )
+
+
+        self.amount_input = TextInput(
+            hint_text="Enter amount",
             multiline=False,
             input_filter="float"
         )
 
-        self.percent = TextInput(
-            hint_text="Percentage",
+
+        self.percent_input = TextInput(
+            hint_text="Enter percentage",
             multiline=False,
             input_filter="float"
         )
+
+
+        calculate_btn = Button(
+            text="Calculate",
+            size_hint=(1, 0.3)
+        )
+
+        calculate_btn.bind(
+            on_press=self.calculate
+        )
+
 
         self.result = Label(
             text="Result: 0",
             font_size=24
         )
 
-        btn = Button(
-            text="Calculate",
-            size_hint=(1, None),
-            height=55
-        )
 
-        btn.bind(on_press=self.calculate)
-
-        self.add_widget(self.amount)
-        self.add_widget(self.percent)
-        self.add_widget(btn)
+        self.add_widget(title)
+        self.add_widget(self.amount_input)
+        self.add_widget(self.percent_input)
+        self.add_widget(calculate_btn)
         self.add_widget(self.result)
 
+
+
     def calculate(self, instance):
+
         try:
-            amount = float(self.amount.text)
-            percent = float(self.percent.text)
+            amount = float(self.amount_input.text)
+            percent = float(self.percent_input.text)
 
-            value = amount * percent / 100
+            result = amount * percent / 100
 
-            self.result.text = f"Result: {value:.2f}"
+            self.result.text = f"Result: {result}"
 
-        except ValueError:
-            self.result.text = "Please enter valid numbers."
+        except:
+            self.result.text = "Invalid input"
