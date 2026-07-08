e083838c85aafd674832e5e961cf66b9bc44df38
new333111 <new333713@gmail.com>
2026-07-09 01:01:21 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivymd.app import MDApp
 
+from app.utils.loader import load_kv_files
 from app.screens.manager import AppManager
 
 
@@ -12,6 +13,8 @@ class PercentageApp(MDApp):
         self.theme_cls.primary_palette = "Blue"
 
         self.theme_cls.theme_style = "Light"
+
+        load_kv_files()
 
         return AppManager()
 
