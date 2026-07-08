3ba1c2fbb3fd211cacb03bd93cd294909fc86b21
new333111 <new333713@gmail.com>
2026-07-09 01:16:53 +0300

    update app/screens/splash.py

--- app/screens/splash.py
+++ app/screens/splash.py
@@ -1,11 +1,19 @@
-from kivymd.uix.screen import MDScreen
 from kivy.clock import Clock
+from kivymd.uix.screen import MDScreen
+
+from app.config import Config
 
 
 class SplashScreen(MDScreen):
 
     def on_enter(self, *args):
-        Clock.schedule_once(self.goto_home, 2)
+        """
+        يتم استدعاؤها عند دخول شاشة البداية.
+        """
+        Clock.schedule_once(self.goto_home, Config.SPLASH_TIME)
 
     def goto_home(self, dt):
+        """
+        الانتقال إلى الشاشة الرئيسية.
+        """
         self.manager.current = "home"
