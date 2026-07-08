97c6bdf5688c9c6c66558f0623bf03e35ebf3b6c
new333111 <new333713@gmail.com>
2026-07-09 00:49:41 +0300

    update app/screens/home.py

--- app/screens/home.py
+++ app/screens/home.py
@@ -1,18 +1,6 @@
 from kivy.uix.screenmanager import Screen
-from services.calculator import Calculator
 
 
 class HomeScreen(Screen):
-
-    def calculate(self):
-
-        try:
-            amount = float(self.ids.amount.text)
-            percent = float(self.ids.percent.text)
-
-            result = Calculator.percentage(amount, percent)
-
-            self.ids.result.text = f"Result: {result:.2f}"
-
-        except ValueError:
-            self.ids.result.text = "Invalid Input"
+    """الشاشة الرئيسية"""
+    pass
